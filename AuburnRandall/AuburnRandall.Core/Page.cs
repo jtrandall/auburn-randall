008f871aee0f0a6b03937513273b0ce12579fb07
Jeremy Randall <jrandall@gfsd.net>
2018-04-12 14:50:35 -0400

    update

---
+++ AuburnRandall/AuburnRandall.Core/Page.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuburnRandall.Core
+{
+    public class Page
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Subheader { get; set; }
+        public string Summary { get; set; }
+        public string Icon { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ AuburnRandall/AuburnRandall.Core/Repositories/PageRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuburnRandall.Core.Repositories
+{
+    public class PageRepository
+    {
+        public static IEnumerable<Page> GetPages()
+        {
+            var pages = new List<Page>
+            {
+                new Page()
+                {
+                    Title = "Projects",
+                    Subheader = "Things that I've been working on",
+                    Summary = "This is the content of the card",
+                    Icon = "build",
+                    Url = "/Home/Projects"
+                },
+
+                new Page()
+                {
+                    Title = "Articles",
+                    Subheader = "Things that I've written about",
+                    Summary = "This is the content of the card",
+                    Icon = "lightbulb_outline",
+                    Url = "/Home/Articles"
+                },
+
+                new Page()
+                {
+                    Title = "About Me",
+                    Subheader = "A little about myself",
+                    Summary = "This is the content of the card",
+                    Icon = "person",
+                    Url = "/Home/About"
+                }
+            };
+
+            return pages;
+        }
+    }
+}
